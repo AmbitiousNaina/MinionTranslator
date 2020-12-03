966a517b08787d86a579f0574b6f82570444fedf
AmbitiousNaina <74875626+AmbitiousNaina@users.noreply.github.com>
2020-12-03 17:29:30 +0530

    minor change

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function clickHandler() {
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translatedText = json.contents.translated;
+            const translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
         })
         .catch(errorHandler)
